be87c772e83a995bc9b74de941e363d518ff9a3d
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-17 17:06:03 +0600

    Project Set Up Done.

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Medi Camp</h1>
+      <p>
+        Your health is our priority. Explore our services and get the best
+        medical care.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
