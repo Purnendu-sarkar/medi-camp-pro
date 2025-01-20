af5ceb385254ff017b73a54552a1fdeb195be3b6
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-20 17:45:36 +0600

    Add Popular Medical Camps section to showcase top camps

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import CampCard from "../Shared/CampCard";
+import useCamps from "../../hooks/useCamps";
 
 const Home = () => {
+  const [camps, loading, refetch] = useCamps();
+  const popularCamps = camps
+    .sort((a, b) => b.participantCount - a.participantCount)
+    .slice(0, 6);
   return (
-    <div className="pt-20">
-      <h1>Welcome to Medi Camp</h1>
-      <p>
-        Your health is our priority. Explore our services and get the best
-        medical care.
-      </p>
+    <div className="">
+      <div className="max-w-7xl mx-auto px-4 py-8 p-6">
+        <h1 className="text-2xl font-bold text-center mb-10">
+          Popular Medical Camps
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {popularCamps.map((camp) => (
+            <CampCard key={camp._id} camp={camp} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
